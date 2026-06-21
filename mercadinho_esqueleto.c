879a00d170670521c99091adcf217d03fa430b83
3n-z0 <enzoamorim1209@gmail.com>
2026-06-20 22:31:02 -0300

    Implement product management functions in estoque
    Added functions to modify price, perform sales, and consult stock.

--- mercadinho_esqueleto.c
+++ mercadinho_esqueleto.c
@@ -59,6 +59,31 @@ void inserirProduto(Estoque *estoque, char *nome, int quantidade, float preco){
     strcpy(estoque->produtos[estoque->total].nome, nome);
     estoque->total++;
 }
+void modificarPreco(Estoque *estoque, int codigo, float novoPreco){
+    estoque->produtos[codigo].preco=novoPreco;
+}
+void realizarVenda(Estoque *estoque){
+    int k; //guarda temporariamente cada codigo do produto
+    float total=0.0;//valor total da compra
+    while(k!=-1){
+        scanf(" %d",&k);
+        printf("%s ",estoque->produtos[k].nome);
+        printf("%d\n",estoque->produtos[k].preco);
+        total+=estoque->produtos[k].preco;
+        estoque->produtos[k].quantidade-=1;//diminui a quantidade do produto em 1
+    }
+    printf("Total: %.2f\n");
+    printf("--------------------------------------------------\n");
+
+}
+void consultarEstoque(Estoque *estoque){
+    for(int i=0;i<(estoque->total);i++){
+        printf("%d ",estoque->produtos[i].codigo);
+        printf("%s ",estoque->produtos[i].nome);
+        printf("%d\n",estoque->produtos[i].quantidade);
+    }
+    printf("--------------------------------------------------\n");
+}
 
 int main() {
     Estoque estoque;
